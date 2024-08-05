8e97b97b50bd55fc56ee5bcd3a861e48a909ebb6
yash-op7 <yashm3375@gmail.com>
2024-08-05 12:33:50 +0530

    spring init

---
+++ better-reads-data-loader/src/main/java/com/yash95/better_reads_data_loader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.yash95.better_reads_data_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+}
